b9845d784e76a371ef330d8feeb659e56bc8f9c8
MatrixEditor <58256046+MatrixEditor@users.noreply.github.com>
2025-04-09 21:18:50 +0200

    Added NBT-NS poisoner ---
    + Updated Python files and added license headers

--- dementor/config.py
+++ dementor/config.py
@@ -1,9 +1,27 @@
-import argparse
+# Copyright (c) 2025 MatrixEditor
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 import tomllib
 import os
 import asyncio
 
-from typing import Any, List, Optional, Tuple
+from typing import Any, List, Optional
 
 from dementor.paths import ASSETS_PATH, CONFIG_PATH, DEMENTOR_PATH
 from dementor.database import DementorDB
@@ -14,7 +32,7 @@ _LOCAL = object()
 
 class TomlConfig:
     _section_: str | None
-    _fields_: list[Tuple[str, str, Any]]
+    _fields_: list[tuple]
 
     def __init__(self, config: dict) -> None:
         for field in self._fields_:
